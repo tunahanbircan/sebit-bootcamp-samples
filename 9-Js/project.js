a7947b11ffbd8fb7eeb8e6069cf895d4e43a09fc
Tunahan Bircan <tunahanbircan42@gmail.com>
2022-07-23 16:31:43 +0300

    add js example

---
+++ 9-Js/project.js
@@ -0,0 +1,37 @@
+//var
+//let
+//const
+
+let gun = 20;
+let ay = 10;
+let yıl = 1997;
+
+console.log(gun, ay, yıl);
+gun = 15;
+console.log(
+  "Tunahan Bircan " + gun + "/" + ay + "/" + yıl + " tarihinde doğmuştur"
+);
+
+const ikinciElAraba = 200000;
+// ikinciElAraba =130000;
+console.log(ikinciElAraba);
+
+var a = 5;
+var a = 3;
+console.log(a);
+
+var c = 5;
+console.log("C değeri dışardayken", c);
+{
+  var c = 3;
+  console.log("C değeri içerideyken", c);
+}
+console.log("C değeri dışarıdayken", c);
+
+let d = 5;
+console.log("D değeri dışardayken", d);
+{
+  let d = 3;
+  console.log("D değeri içerideyken", d);
+}
+console.log("D değeri dışarıdayken", d);
